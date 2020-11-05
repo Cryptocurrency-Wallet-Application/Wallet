dfcc4d58f4ec1e00829e8d206190264e27d50c6c
bartutiryaki <56831203+bartutiryaki@users.noreply.github.com>
2020-11-05 14:58:18 +0300

    Add files via upload

---
+++ app/src/main/java/com/example/comp491/MainPageFragment.java
@@ -0,0 +1,49 @@
+package com.example.comp491;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+
+public class MainPageFragment extends Fragment {
+
+    @BindView(R.id.userName)
+    EditText userName;
+
+    @BindView(R.id.password)
+    EditText password;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        final View rootView = inflater.inflate(R.layout.main_page, container, false);
+        ButterKnife.bind(this,rootView);
+
+        return rootView;
+    }
+
+    @OnClick(R.id.loginButton)
+    void login() {
+
+        if (userName.getText().equals("") || userName == null || password.getText().equals("") || password == null) { //add check username from system
+            ((MainActivity)this.getActivity()).alert("Login", "Username or Password wrong.", false);
+            return;
+        }
+
+        //add menu fragment
+
+    }
+
+}
